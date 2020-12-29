2fd8b974115a2e9b2826a9ac308b989dc6988821
ha49 <kaysersoza@outlook.com>
2020-12-29 11:03:11 +0100

    calculator app added as a separate react app

--- 2-calculator/src/components/CalculatorComponent.jsx
+++ 2-calculator/src/components/CalculatorComponent.jsx
@@ -4,14 +4,15 @@ import './Calculator.css'
 class CalculatorComponent extends Component {
 
 
-constructor() {
-    super();
-    this.state={
-        sum:0
+    constructor() {
+        super();
+        this.state = {
+            sum: 0,
+            sum2: 0
 
+        }
+        this.calculate = this.calculate.bind(this)
     }
-}
-
 
     render() {
         return (
@@ -19,13 +20,22 @@ constructor() {
 
                 <button onClick={this.calculate}>+1</button>
                 <span className="result">{this.state.sum}</span>
+                <span className="result">{this.state.sum2}</span>
 
             </div>
         )
     }
 
-    calculate(){
-        console.log("calculate")
+    calculate() {
+
+        this.setState(
+            {
+                sum: this.state.sum +1,
+                sum2: this.state.sum2 +1
+
+            }
+            )
+        console.log("sum:"+(this.state.sum+1))
     }
 }
 
